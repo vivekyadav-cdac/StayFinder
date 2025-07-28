ffddccc7be37e47248e035fb4f580b51d1e26cb7
KshitijBangde <kshitijbangde2110@gmail.com>
2025-07-28 17:47:45 +0530

    Auth-service Done

--- backend/sf-auth/Auth-Service/src/main/java/com/stayfinder/Auth_Service/models/User.java
+++ backend/sf-auth/Auth-Service/src/main/java/com/stayfinder/Auth_Service/models/User.java
@@ -1,5 +1,6 @@
 package com.stayfinder.Auth_Service.models;
 
+import java.io.Serial;
 import java.util.Collection;
 import java.util.List;
 
@@ -30,6 +31,7 @@ public class User implements UserDetails {
     /**
      *
      */
+    @Serial
     private static final long serialVersionUID = 4475362821305523304L;
 
     @Id
