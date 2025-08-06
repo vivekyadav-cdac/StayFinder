48c1204128e4f6864d3e6730c6e8390d1cfa5d2a
vivekyadav-cdac <vivekyadavcdac@gmail.com>
2025-08-07 05:23:50 +0530

    services pgproperty and bookingmanagement updated and running with new databases on neon

--- backend/sf-propertymanagement/src/main/java/com/cdacproject/stayfinder/pg_property_service/mapper/RoomMapper.java
+++ backend/sf-propertymanagement/src/main/java/com/cdacproject/stayfinder/pg_property_service/mapper/RoomMapper.java
@@ -9,9 +9,20 @@ import org.mapstruct.*;
 public interface RoomMapper {
 
     @Mapping(target = "imageUrl", source = "imageUrl", qualifiedByName = "buildImageUrl")
+    @Mapping(target = "pgId", source = "pg.id")
     RoomResponseDto toResponseDto(Room room, @Context String gatewayUrl);
 
+    @Mapping(target = "pg", source = "pgId")  // Tells MapStruct to use the mapPgId method
     Room toEntity(RoomDto dto);
+
+    // Custom mapping from pgId → PG
+    default com.cdacproject.stayfinder.pg_property_service.model.PG mapPgId(Long pgId) {
+        if (pgId == null) return null;
+        com.cdacproject.stayfinder.pg_property_service.model.PG pg = new com.cdacproject.stayfinder.pg_property_service.model.PG();
+        pg.setId(pgId);
+        return pg;
+    }
 }
+
 
 
